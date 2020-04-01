d96408d29d831880de56b8d02a86f8ad2858484a
szymonbig <szymonbig@gmail.com>
2020-04-01 18:39:34 +0200

    Add programme pattern

---
+++ Komis/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Komis.Models;
+using Komis.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Komis.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISamochodRepository _samochodRepository;
+
+        public HomeController(ISamochodRepository samochodRepository)
+        {
+            _samochodRepository = samochodRepository;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var samochody = _samochodRepository.PobierzWszystkieSamochody().OrderBy(s => s.Marka);      //sortowanie OrderBy
+
+            var homeVM = new HomeVM()
+            {
+                Tytul = "Przegląd samochodów",
+                Samochody = samochody.ToList()
+            };
+
+            return View(homeVM);
+        }
+    }
+}
--- Komis/Startup.cs
+++ Komis/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Komis.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,7 +16,8 @@ namespace Komis
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddTransient<ISamochodRepository, MockSamochodRepository>();       //gdy ktoś zapyta o ISamochodRepository to zostanie zwórcone MockSamochodReposiotry            
+            services.AddMvc();                                                          //rejestracja usuług
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
